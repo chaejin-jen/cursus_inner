00e9426e79e7ef89e3673a9c85b9b578c28ce4fc
Chaejin Kim <chaejkim@Chaejinui-Macmini.local>
2022-02-06 14:05:35 +0900

    📦 NEW: ft_printf

--- ft_printf/malloc_bonus/ft_prntchar.c
+++ ft_printf/malloc_bonus/ft_prntchar.c
@@ -1,21 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_prntchar.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chaejkim <chaejkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/17 15:36:41 by chaejkim          #+#    #+#             */
-/*   Updated: 2022/02/04 02:14:33 by chaejkim         ###   ########.fr       */
+/*   Created: 2022/01/27 21:33:12 by chaejkim          #+#    #+#             */
+/*   Updated: 2022/02/04 21:45:58 by chaejkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_printf.h"
 
-int	ft_isdigit(int c)
+int	print_c(t_print_info p_info, int c)
 {
-	if (c > 47 && c < 58)
-		return (1);
-	else
-		return (0);
+	//unsigned char	*str;
+	unsigned char	u_c;
+
+	p_info.type = 'c';
+	u_c = (unsigned char)c;
+	return (write(1, &u_c, 1));  
+}
+
+int	print_s(t_print_info p_info, char *s)
+{
+	size_t	len;
+
+	p_info.type = 's';
+	len = 0;
+	if (s == NULL)
+		s = "(null)";
+	while (s[len])
+		len++;
+	write(1, s, len);
+	return (len);
 }
