35a1692316797b821844bbece9fa8414894e0896
Chaejin Kim <chaejkim@Chaejinui-Macmini.local>
2022-03-07 14:30:58 +0900

    📦 NEW: example_mlx

---
+++ example/mlx/etc/mlx_example.h
@@ -0,0 +1,14 @@
+#ifndef MLX_EXAMPLE_H
+# define MLX_EXAMPLE_H
+
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
+#endif
