d02ff982d9b563a5501820429c4df08370741acc
chaejin-jen <jen71324@gmail.com>
2022-10-25 09:19:59 +0900

    New : cpp01 - ex02

---
+++ circle_04/cpp-module/cpp01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int	main(void)
+{
+	std::string		string = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+
+	std::cout << "string(address)    : " << &string << '\n';
+	std::cout << "stringPTR(address) : " << stringPTR << '\n';
+	std::cout << "stringREF(address) : " << &stringREF << std::endl;
+
+	std::cout << "stringPTR(string) : " << *stringPTR << '\n';
+	std::cout << "stringREF(string) : " << stringREF << std::endl;
+
+	return 0;
+}
