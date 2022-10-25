5b8a89311a1adc036ac9bab64ed585cf75d0a26c
chaejin-jen <jen71324@gmail.com>
2022-10-25 09:04:35 +0900

    Fix : cpp01 - ex00

--- circle_04/cpp-module/cpp01/ex00/Zombie.cpp
+++ circle_04/cpp-module/cpp01/ex00/Zombie.cpp
@@ -1,10 +1,13 @@
 #include "Zombie.hpp"
 
-Zombie::Zombie( void ) : _name("unknown") { }
-Zombie::Zombie( std::string name ) : _name(name) { }
-Zombie::~Zombie( void ) {
-	std::cout << _name << " is completely dead!" << std::endl;
+Zombie::Zombie( std::string name ) : _name(name) {
+	std::cout << _name << " is born!" << std::endl;
 }
+
+Zombie::~Zombie( void ) {
+	std::cout << _name << " died!" << std::endl;
+}
+
 void Zombie::announce(void) const
 {
 	std::cout << _name << " BraiiiiiiinnnzzzZ..." << std::endl;
--- circle_04/cpp-module/cpp01/ex00/Zombie.hpp
+++ circle_04/cpp-module/cpp01/ex00/Zombie.hpp
@@ -11,7 +11,6 @@
 class Zombie {
 
 public:
-	Zombie( void );
 	Zombie( std::string name );
 	~Zombie( void );
 
