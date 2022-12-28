d123b7e31b5edbea430d0a7891df5afe712df17b
chaejin-jen <jen71324@gmail.com>
2022-12-29 03:24:03 +0900

    🐛 FIX: cpp01 - ex03 init _weapon of Human classes as NULL

--- circle_04/cpp-module/cpp01/ex03/HumanB.cpp
+++ circle_04/cpp-module/cpp01/ex03/HumanB.cpp
@@ -1,20 +1,24 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB( std::string name) : _name(name) {
-	std::cout << "(appear)" << this->_name << std::endl;
+HumanB::HumanB( std::string name) : _name(name), _weapon(NULL) {
+	// std::cout << "\033[0;90m(appear)" << this->_name << "\033[0m" << std::endl;
 }
 
 HumanB::~HumanB() {
-	std::cout << "(disappear)" << this->_name << std::endl;
+	// std::cout << "\033[0;90m(disappear)" << this->_name << "\033[0m" << std::endl;
 }
 
 void HumanB::setWeapon(Weapon& weapon)
 {
 	this->_weapon = &weapon;
-	std::cout << "(change)" << this->_name << " - " << weapon.getType() << std::endl;	
+	// std::cout << "\033[0;92m(change)" << this->_name << " - " << weapon.getType() << "\033[0m" << std::endl;	
 }
 
 void HumanB::attack(void) const
 {
-	std::cout << this->_name << " with his " << this->_weapon->getType() << std::endl;	
+	std::cout << this->_name;
+	if (this->_weapon) 
+		std::cout << " attacks with their " << this->_weapon->getType() << std::endl;
+	else
+		std::cout << " couldn't attack without weapon" << std::endl;
 }
