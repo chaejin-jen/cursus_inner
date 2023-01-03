d87b64b14e3cdc2f5ea653adb26b99f63a8fd212
chaejin-jen <jen71324@gmail.com>
2023-01-03 15:24:47 +0900

    🐛 FIX: cpp03 - virtual destructor

--- circle_04/cpp-module/cpp03/ex01/ClapTrap.hpp
+++ circle_04/cpp-module/cpp03/ex01/ClapTrap.hpp
@@ -23,7 +23,7 @@ class ClapTrap {
 		ClapTrap(std::string name);
 		ClapTrap(const ClapTrap& other);
 		ClapTrap& operator=(const ClapTrap& other);
-		~ClapTrap(void);
+		virtual ~ClapTrap(void);
 		void attack(const std::string& target);
 		void takeDamage(unsigned int amount);
 		void beRepaired(unsigned int amount);
