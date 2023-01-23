b738e8081375bbf4870402342c6ef660ddc355d8
chaejin-jen <jen71324@gmail.com>
2023-01-23 12:10:08 +0900

    🚚 MOVE: ft_containers - renamed as ft_iterator

--- circle_05/ft_containers/ft_iterator.hpp
+++ circle_05/ft_containers/ft_iterator.hpp
@@ -2,12 +2,108 @@
 #ifndef __ITERATOR_H__
 #define __ITERATOR_H__
 
+
+/*
+    iterator synopsis
+namespace ft {
+
+template<class Iterator> struct iterator_traits;
+
+template<class T> struct iterator_traits<T*>;
+
+template<class T> struct iterator_traits<const T*>;
+
+template<class Category, class T, class Distance = std::ptrdiff_t,
+	class Pointer = T*, class Reference = T&> struct iterator;
+
+struct input_iterator_tag {};
+struct output_iterator_tag {};
+struct forward_iterator_tag: public input_iterator_tag {};
+struct bidirectional_iterator_tag: public forward_iterator_tag {};
+struct random_access_iterator_tag: public bidirectional_iterator_tag {};
+
+template <class InputIterator, class Distance>
+	void advance(InputIterator& i, Distance n);
+template <class InputIterator>
+	typename iterator_traits<InputIterator>::difference_type
+		distance(InputIterator first, InputIterator last);
+
+template <class Iterator> class reverse_iterator : public
+	iterator<typename iterator_traits<Iterator>::iterator_category,
+		typename iterator_traits<Iterator>::value_type,
+		typename iterator_traits<Iterator>::difference_type,
+		typename iterator_traits<Iterator>::pointer,
+		typename iterator_traits<Iterator>::reference> {
+protected:
+	Iterator current;
+public:
+	typedef Iterator iterator_type;
+	typedef typename iterator_traits<Iterator>::difference_type difference_type;
+	typedef typename iterator_traits<Iterator>::reference reference;
+	typedef typename iterator_traits<Iterator>::pointer pointer;
+
+	reverse_iterator();
+	explicit reverse_iterator(Iterator x);
+	template <class U>
+	reverse_iterator(const reverse_iterator<U>& u);
+
+	Iterator base() const; // explicit
+	reference operator*() const;
+	pointer operator->() const;
+
+	reverse_iterator& operator++();
+	reverse_iterator operator++(int);
+	reverse_iterator& operator--();
+	reverse_iterator operator--(int);
+
+	reverse_iterator operator+ (difference_type n) const;
+	reverse_iterator& operator+=(difference_type n);
+	reverse_iterator operator- (difference_type n) const;
+	reverse_iterator& operator-=(difference_type n);
+	reference operator[](difference_type n) const;
+};
+
+template <class Iterator>
+	bool operator==(
+		const reverse_iterator<Iterator>& x,
+		const reverse_iterator<Iterator>& y);
+template <class Iterator>
+	bool operator<(
+		const reverse_iterator<Iterator>& x,
+		const reverse_iterator<Iterator>& y);
+template <class Iterator>
+	bool operator!=(
+		const reverse_iterator<Iterator>& x,
+		const reverse_iterator<Iterator>& y);
+template <class Iterator>
+	bool operator>(
+		const reverse_iterator<Iterator>& x,
+		const reverse_iterator<Iterator>& y);
+template <class Iterator>
+	bool operator>=(
+		const reverse_iterator<Iterator>& x,
+		const reverse_iterator<Iterator>& y);
+template <class Iterator>
+	bool operator<=(
+		const reverse_iterator<Iterator>& x,
+		const reverse_iterator<Iterator>& y);
+
+template <class Iterator>
+	typename reverse_iterator<Iterator>::difference_type operator-(
+			const reverse_iterator<Iterator>& x,
+			const reverse_iterator<Iterator>& y);
+template <class Iterator>
+	reverse_iterator<Iterator> operator+(
+		typename reverse_iterator<Iterator>::difference_type n,
+		const reverse_iterator<Iterator>& x);
+
+}
+*/
+
 #include <cstddef>
-#include <iosfwd>
 
 namespace ft {
 
-// primitives:
 template<class Iterator> struct iterator_traits {
 	typedef typename Iterator::difference_type difference_type;
 	typedef typename Iterator::value_type value_type;
@@ -47,14 +143,6 @@ struct forward_iterator_tag: public input_iterator_tag {};
 struct bidirectional_iterator_tag: public forward_iterator_tag {};
 struct random_access_iterator_tag: public bidirectional_iterator_tag {};
 
-// iterator operations:
-template <class InputIterator, class Distance>
-	void advance(InputIterator& i, Distance n);
-template <class InputIterator>
-	typename iterator_traits<InputIterator>::difference_type
-		distance(InputIterator first, InputIterator last);
-
-// predefined iterators:
 template <class Iterator> class reverse_iterator : public
 	iterator<typename iterator_traits<Iterator>::iterator_category,
 		typename iterator_traits<Iterator>::value_type,
@@ -168,59 +256,5 @@ template <class Iterator>
 		const reverse_iterator<Iterator>& x){
 			return reverse_iterator<Iterator> (x.current - n)
 		}
-
-template <class Container> class back_insert_iterator;
-template <class Container>
-	back_insert_iterator<Container> back_inserter(Container& x);
-
-template <class Container> class front_insert_iterator;
-template <class Container>
-	front_insert_iterator<Container> front_inserter(Container& x);
-
-template <class Container> class insert_iterator;
-template <class Container, class Iterator>
-	insert_iterator<Container> inserter(Container& x, Iterator i);
-
-// stream iterators:
-template <class T, class charT = char, class traits = std::char_traits<charT>,
-	class Distance = std::ptrdiff_t>
-class istream_iterator;
-template <class T, class charT, class traits, class Distance>
-	bool operator==(const istream_iterator<T,charT,traits,Distance>& x,
-		const istream_iterator<T,charT,traits,Distance>& y);
-template <class T, class charT, class traits, class Distance>
-	bool operator!=(const istream_iterator<T,charT,traits,Distance>& x,
-		const istream_iterator<T,charT,traits,Distance>& y);
-
-template <class T, class charT = char, class traits = std::char_traits<charT> >
-	class ostream_iterator;
-
-template<class charT, class traits = std::char_traits<charT> >
-	class istreambuf_iterator;
-template <class charT, class traits>
-	bool operator==(const istreambuf_iterator<charT,traits>& a,
-		const istreambuf_iterator<charT,traits>& b);
-template <class charT, class traits>
-	bool operator!=(const istreambuf_iterator<charT,traits>& a,
-		const istreambuf_iterator<charT,traits>& b);
-
-template <class charT, class traits = std::char_traits<charT> >
-	class ostreambuf_iterator;
-
-/* reverse function
-template <class BidirectionalIterator>
-void reverse(BidirectionalIterator first, BidirectionalIterator last) {
-	typename iterator_traits<BidirectionalIterator>::difference_type n =
-		distance(first, last);
-	--n;
-	while(n > 0) {
-		typename iterator_traits<BidirectionalIterator>::value_type
-		tmp = *first;
-		*first++ = *--last;
-		*last = tmp;
-		n -= 2;
-	}
-}
-*/
 
 }
