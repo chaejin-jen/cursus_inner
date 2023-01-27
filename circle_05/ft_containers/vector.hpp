894cca36ff3565226f661d3cae057e30d597dce5
chaejin-jen <jen71324@gmail.com>
2023-01-27 20:32:16 +0900

    ♻️REFACTOR: ft_containers - header convection

--- circle_05/ft_containers/vector.hpp
+++ circle_05/ft_containers/vector.hpp
@@ -270,59 +270,175 @@ public:
 	typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
 
 	// construct/copy/destroy:
-	explicit vector(const Allocator & = Allocator());
+	explicit vector(const Allocator & = Allocator()){
+		create();
+	}
 	explicit vector(size_type n, const T& value = T(),
-		const Allocator & = Allocator());
+		const Allocator & = Allocator()) {
+			create(n, value);
+		}
 	template <typename InputIterator>
 	vector(InputIterator first, InputIterator last,
-		const Allocator & = Allocator());
-	vector(const vector<T, Allocator>& x);
-	~vector();
-	vector<T, Allocator>& operator=(const vector<T, Allocator>& x);
+		const Allocator & = Allocator()) {
+			create(first, last);
+		}
+	vector(const vector<T, Allocator>& x) {
+			create(x.begin(), x.end());
+		}
+	~vector() {
+		destroy();
+	}
+	vector<T, Allocator>& operator=(const vector<T, Allocator>& x){
+		if (&x != this) {
+			destroy();
+			create(x.begin(), x.end());
+		}
+		return *this;
+	}
 	template <typename InputIterator>
-	void assign(InputIterator first, InputIterator last);
+	void assign(InputIterator first, InputIterator last){
+		//erase(begin(), end());
+		//insert(begin(), first, last)
+	}
 	void assign(size_type n, const T& u);
-	allocator_type get_allocator() const;
+	allocator_type get_allocator() const{
+		//erase(begin(), end());
+		//insert(begin(), n, t);
+	}
 
 	// iterators:
-	iterator begin();
-	const_iterator begin() const;
-	iterator end();
-	const_iterator end() const;
-	reverse_iterator rbegin();
-	const_reverse_iterator rbegin() const;
-	reverse_iterator rend();
-	const_reverse_iterator rend() const;
+	iterator begin(){
+		return iterator(start_);
+	}
+	const_iterator begin() const{
+		return const_iterator(start_);
+	}
+	iterator end(){
+		return iterator(finish_);
+	}
+	const_iterator end() const{
+		return const_iterator(finish_);
+	}
+	reverse_iterator rbegin(){
+		return reverse_iterator(finish_);
+	}
+	const_reverse_iterator rbegin() const{
+		return const_reverse_iterator(finish_);
+	}
+	reverse_iterator rend(){
+		return reverse_iterator(start_);
+	}
+	const_reverse_iterator rend() const{
+		return const_reverse_iterator(start_);
+	}
 
 	// capacity:
-	size_type size() const;
-	size_type max_size() const;
-	void resize(size_type sz, T c = T());
-	size_type capacity() const;
-	bool empty() const;
-	void reserve(size_type n);
+	size_type size() const{
+		return static_cast<size_type>(end() - begin());
+	}
+	size_type max_size() const{
+		return size_type(-1) / sizeof(T);
+	}
+	void resize(size_type sz, T c = T()){
+		//if (sz > size())
+		//	insert(end(), sz - size(), c);
+		//else if (sz < size())
+		//	erase(begin() + sz, end());
+		//else
+		//	;
+	}
+	size_type capacity() const{
+		return size_type(end_of_storage_ - begin());
+	}
+	bool empty() const{
+		return begin() == end();
+	}
+	void reserve(size_type n){
+		if (n > max_size())
+			throw ::std::length_error("over max_size");
+		if (capacity() < n)
+		{
+			const size_type old_size = size();
+			allocator_type new_alloc;
+			iterator new_iter = new_alloc.allocate(n);
+			::std::swap(alloc_, new_alloc);
+			::std::swap(start_, new_iter);
+			finish_ = start_;
+			construct(new_iter, new_iter + old_size);
+			destroy();
+			::std::swap(alloc_, new_alloc);
+			::std::swap(start_, new_iter);
+			finish_ = start_ + old_size;
+			end_of_storage_ = start_ + n;
+		}
+	}
 
 	// element access:
-	reference operator[](size_type n);
-	const_reference operator[](size_type n) const;
-	const_reference at(size_type n) const;
-	reference at(size_type n);
-	reference front();
-	const_reference front() const;
-	reference back();
-	const_reference back() const;
+	reference operator[](size_type n){
+
+	}
+	const_reference operator[](size_type n) const{
+
+	}
+	const_reference at(size_type n) const{
+
+	}
+	reference at(size_type n){
+
+	}
+	reference front(){
+
+	}
+	const_reference front() const{
+
+	}
+	reference back(){
+
+	}
+	const_reference back() const{
+
+	}
 
 	// modifiers:
-	void push_back(const T& x);
-	void pop_back();
-	iterator insert(iterator position, const T& x);
-	void insert(iterator position, size_type n, const T& x);
+	void push_back(const T& x){
+
+	}
+	void pop_back(){
+
+	}
+	iterator insert(iterator position, const T& x){
+
+	}
+	void insert(iterator position, size_type n, const T& x){
+
+	}
 	template <typename InputIterator>
-	void insert(iterator position, InputIterator first, InputIterator last);
-	iterator erase(iterator position);
-	iterator erase(iterator first, iterator last);
-	void swap(vector<T, Allocator>&);
-	void clear();
+	void insert(iterator position, InputIterator first, InputIterator last){
+
+	}
+	iterator erase(iterator position){
+
+	}
+	iterator erase(iterator first, iterator last){
+		//throws : Nothing unless an exception is thrown by the copy constructor or assignment operator of T.
+	}
+	void swap(vector<T, Allocator>& x){
+		allocator_type alloc(alloc_);
+		iterator start(start_);
+		iterator finish(finish_);
+		iterator end_of_storage(end_of_storage_);
+		alloc_ = x.alloc_;
+		start_ = x.start_;
+		finish_ = x.finish_;
+		end_of_storage_ = x.end_of_storage_;
+		x.alloc_ = alloc;
+		x.start_ = start;
+		x.finish_ = finish;
+		x.end_of_storage_ = end_of_storage;
+	}
+	void clear(){
+		destroy();
+	}
 
 protected:
 	allocator_type alloc_;
@@ -331,48 +447,85 @@ protected:
 	pointer end_of_storage_;
 
 private:
-	void create();
-	void create(size_type n, const T& value);
-	void create(const_iterator first, const_iterator last);
-	void construct(size_type n, const T& value);
-	void construct(const_iterator first, const_iterator last);
-	void destroy();
+	void create(){
+		start_ = finish_ = end_of_storage_ = 0;
+	}
+	void create(size_type n, const T& value){
+		if (n > max_size())
+			throw ::std::length_error("over max_size");
+		start_ = finish_ = alloc_.allocate(n);
+		end_of_storage_ = start_ + n;
+		construct(n, value);
+	}
+	void create(const_iterator first, const_iterator last){
+		if (last - first > max_size())
+			throw ::std::length_error("over max_size");
+		start_ = finish_ = alloc_.allocate(last - first);
+		end_of_storage_ = start_ + (last - first);
+		construct(first, last);
+	}
+
+	void construct(size_type n, const T& value){
+		if (n > max_size())
+			throw ::std::length_error("over max_size");
+		do{
+			alloc_.construct(finish_, value);
+			++finish_;
+			--n;
+		}while (n > 0);
+	}
+	void construct(const_iterator first, const_iterator last){
+		size_t i(0);
+
+		if (first > last)
+			return ; // exception
+		do{
+			alloc_.construct(finish_, *(first + i));
+			++finish_;
+			++i;
+		}while ((first + i) != last);
+	}
+	void destroy(){
+			if (start_)
+		{
+			vector::iterator it = finish_;
+			while (it != start_) {
+				alloc_.destroy(--it);
+			}
+			alloc_.deallocate(start_, end_of_storage_ - start_);
+		}
+		start_ = finish_ = end_of_storage_ = 0;
+	}
 
 };
 
 //// operator:
 //template <typename T, typename Allocator>
-//bool operator==(const vector<T, Allocator>& x, const vector<T, Allocator>& y)
-//{
+//bool operator==(const vector<T, Allocator>& x, const vector<T, Allocator>& y){
 //}
 
 //template <typename T, typename Allocator>
-//bool operator< (const vector<T, Allocator>& x, const vector<T, Allocator>& y)
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//bool operator!=(const vector<T, Allocator>& x, const vector<T, Allocator>& y)
-//{
+//bool operator< (const vector<T, Allocator>& x, const vector<T, Allocator>& y){
 
 //}
 
 //template <typename T, typename Allocator>
-//bool operator> (const vector<T, Allocator>& x, const vector<T, Allocator>& y)
-//{
+//bool operator!=(const vector<T, Allocator>& x, const vector<T, Allocator>& y){
 
 //}
 
 //template <typename T, typename Allocator>
-//bool operator>=(const vector<T, Allocator>& x, const vector<T, Allocator>& y)
-//{
+//bool operator> (const vector<T, Allocator>& x, const vector<T, Allocator>& y){
 
 //}
 
 //template <typename T, typename Allocator>
-//bool operator<=(const vector<T, Allocator>& x, const vector<T, Allocator>& y)
-//{
+//bool operator>=(const vector<T, Allocator>& x, const vector<T, Allocator>& y){
+
+//}
+
+//template <typename T, typename Allocator>
+//bool operator<=(const vector<T, Allocator>& x, const vector<T, Allocator>& y){
 
 //}
 
@@ -383,360 +536,6 @@ void swap(vector<T, Allocator>& x, vector<T, Allocator>& y)
 	x.swap(y);
 }
 
-
-// construct/copy/destroy:
-template <typename T, typename Allocator>
-vector<T, Allocator>::vector(const Allocator &){ create(); }
-
-template <typename T, typename Allocator>
-vector<T, Allocator>::vector(size_type n, const T& value, const Allocator &)
-{ create(n, value); }
-
-template <typename T, typename Allocator>
-template <typename InputIterator>
-vector<T, Allocator>::vector(InputIterator first, InputIterator last, const Allocator &)
-{ create(first, last); }
-
-template <typename T, typename Allocator>
-vector<T, Allocator>::vector(const vector<T, Allocator>& x)
-{ create(x.begin(), x.end()); }
-
-template <typename T, typename Allocator>
-vector<T, Allocator>::~vector(){ destroy(); }
-
-template <typename T, typename Allocator>
-vector<T, Allocator>& vector<T, Allocator>::operator=(const vector<T, Allocator>& x)
-{
-	if (&x != this) {
-		destroy();
-		create(x.begin(), x.end());
-	}
-	return *this;
 }
 
-//template <typename T, typename Allocator>
-//template <typename InputIterator>
-//void vector<T, Allocator>::assign(InputIterator first, InputIterator last)
-//{
-//	//erase(begin(), end());
-//	//insert(begin(), first, last)
-//}
-
-//template <typename T, typename Allocator>
-//void vector<T, Allocator>::assign(size_type n, const T& u)
-//{
-//	//erase(begin(), end());
-//	//insert(begin(), n, t);
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::allocator_type
-//vector<T, Allocator>::get_allocator() const
-//{
-
-//}
-
-// iterators:
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::iterator
-vector<T, Allocator>::begin()
-{
-	return iterator(start_);
-}
-
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::const_iterator
-vector<T, Allocator>::begin() const
-{
-	return const_iterator(start_);
-}
-
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::iterator
-vector<T, Allocator>::end()
-{
-	return iterator(finish_);
-}
-
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::const_iterator
-vector<T, Allocator>::end() const
-{
-	return const_iterator(finish_);
-}
-
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::reverse_iterator
-vector<T, Allocator>::rbegin()
-{
-	return reverse_iterator(finish_);
-}
-
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::const_reverse_iterator
-vector<T, Allocator>::rbegin() const
-{
-	return const_reverse_iterator(finish_);
-}
-
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::reverse_iterator
-vector<T, Allocator>::rend()
-{
-	return reverse_iterator(start_);
-}
-
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::const_reverse_iterator
-vector<T, Allocator>::rend() const
-{
-	return const_reverse_iterator(start_);
-}
-
-// capacity:
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::size_type
-vector<T, Allocator>::size() const
-{ return static_cast<size_type>(end() - begin()); }
-
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::size_type
-vector<T, Allocator>::max_size() const
-{ return size_type(-1) / sizeof(T); }
-
-//template <typename T, typename Allocator>
-//void vector<T, Allocator>::resize(size_type sz, T c)
-//{
-//	if (sz > size())
-//		insert(end(), sz - size(), c);
-//	else if (sz < size())
-//		erase(begin() + sz, end());
-//	else
-//		;
-//}
-
-template <typename T, typename Allocator>
-typename vector<T, Allocator>::size_type
-vector<T, Allocator>::capacity() const
-{ return size_type(end_of_storage_ - begin()); }
-
-template <typename T, typename Allocator>
-bool vector<T, Allocator>::empty() const
-{ return begin() == end(); }
-
-template <typename T, typename Allocator>
-void vector<T, Allocator>::reserve(size_type n)
-{
-	if (n > max_size())
-		throw ::std::length_error("over max_size");
-	if (capacity() < n)
-	{
-		const size_type old_size = size();
-		allocator_type new_alloc;
-		iterator new_iter = new_alloc.allocate(n);
-		::std::swap(alloc_, new_alloc);
-		::std::swap(start_, new_iter);
-		finish_ = start_;
-		construct(new_iter, new_iter + old_size);
-		destroy();
-		::std::swap(alloc_, new_alloc);
-		::std::swap(start_, new_iter);
-		finish_ = start_ + old_size;
-		end_of_storage_ = start_ + n;
-	}
-}
-
-//// element access:
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::reference
-//vector<T, Allocator>::operator[](size_type n)
-//{
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::const_reference
-//vector<T, Allocator>::operator[](size_type n) const
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::const_reference
-//vector<T, Allocator>::at(size_type n) const
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::reference
-//vector<T, Allocator>::at(size_type n)
-//{
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::reference
-//vector<T, Allocator>::front()
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::const_reference
-//vector<T, Allocator>::front() const
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::reference
-//vector<T, Allocator>::back()
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::const_reference
-//vector<T, Allocator>::back() const
-//{
-
-//}
-
-//// modifiers:
-
-//template <typename T, typename Allocator>
-//void vector<T, Allocator>::push_back(const T& x)
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//void vector<T, Allocator>::pop_back()
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::iterator
-//vector<T, Allocator>::insert(iterator position, const T& x)
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//void vector<T, Allocator>::insert(iterator position, size_type n, const T& x)
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//template <typename InputIterator>
-//void vector<T, Allocator>::insert(iterator position, InputIterator first, InputIterator last)
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::iterator
-//vector<T, Allocator>::erase(iterator position)
-//{
-
-//}
-
-//template <typename T, typename Allocator>
-//typename vector<T, Allocator>::iterator
-//vector<T, Allocator>::erase(iterator first, iterator last)
-//{
-//	//throws : Nothing unless an exception is thrown by the copy constructor or assignment operator of T.
-//}
-
-template <typename T, typename Allocator>
-void vector<T, Allocator>::swap(vector<T, Allocator>& x)
-{
-	allocator_type alloc(alloc_);
-	iterator start(start_);
-	iterator finish(finish_);
-	iterator end_of_storage(end_of_storage_);
-	alloc_ = x.alloc_;
-	start_ = x.start_;
-	finish_ = x.finish_;
-	end_of_storage_ = x.end_of_storage_;
-	x.alloc_ = alloc;
-	x.start_ = start;
-	x.finish_ = finish;
-	x.end_of_storage_ = end_of_storage;
-}
-
-template <typename T, typename Allocator>
-void vector<T, Allocator>::clear(){ destroy(); }
-
-// internal:
-
-template <typename T, typename Allocator>
-void vector<T, Allocator>::create()
-{
-	start_ = finish_ = end_of_storage_ = 0;
-}
-
-template <typename T, typename Allocator>
-void vector<T, Allocator>::create(size_type n, const T& value)
-{
-	if (n > max_size())
-		throw ::std::length_error("over max_size");
-	start_ = finish_ = alloc_.allocate(n);
-	end_of_storage_ = start_ + n;
-	construct(n, value);
-}
-
-template <typename T, typename Allocator>
-void vector<T, Allocator>::create(const_iterator first, const_iterator last)
-{
-	if (last - first > max_size())
-		throw ::std::length_error("over max_size");
-	start_ = finish_ = alloc_.allocate(last - first);
-	end_of_storage_ = start_ + (last - first);
-	construct(first, last);
-}
-
-template <typename T, typename Allocator>
-void vector<T, Allocator>::construct(size_type n, const T& value)
-{
-	if (n > max_size())
-		throw ::std::length_error("over max_size");
-	do{
-		alloc_.construct(finish_, value);
-		++finish_;
-		--n;
-	}while (n > 0);
-}
-
-template <typename T, typename Allocator>
-void vector<T, Allocator>::construct(const_iterator first, const_iterator last)
-{
-	size_t i(0);
-
-	if (first > last)
-		return ; // exception
-	do{
-		alloc_.construct(finish_, *(first + i));
-		++finish_;
-		++i;
-	}while ((first + i) != last);
-}
-
-template <typename T, typename Allocator>
-void vector<T, Allocator>::destroy()
-{
-	if (start_)
-	{
-		vector::iterator it = finish_;
-		while (it != start_) {
-			alloc_.destroy(--it);
-		}
-		alloc_.deallocate(start_, end_of_storage_ - start_);
-	}
-	start_ = finish_ = end_of_storage_ = 0;
-}
-
-}
 #endif /* __VECTOR_H__ */
